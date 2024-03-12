f17f4530b98be28ca70289beaa02de1a8bf2778f
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-03-12 15:17:30 +0530

    Create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
