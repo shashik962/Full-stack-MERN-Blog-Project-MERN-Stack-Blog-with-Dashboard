59d7070d423ac23c8745b3f8c4c1d264bc1d7d69
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-03-27 18:20:11 +0530

    Add admin functionality to the user

--- api/models/user-model.js
+++ api/models/user-model.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 }, {timestamps: true}
 );
 
